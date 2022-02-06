832dbd822a42a98338856d31e46343296d300f99
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-02-06 19:01:00 +0100

    Update
    - Added font size variable
    - Started css re-structure

--- js/listeners.js
+++ js/listeners.js
@@ -1,1 +1,12 @@
 addEvent(window, 'unload', save_settings);
+
+addEvent(document.getElementById("edit-box"), 'input', save_settings);
+
+addEvent(document.getElementById("open-btn"), 'click', function () {
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    open_dilg.click();
+  } else {
+    alert('The File APIs are not fully supported in this browser.');
+  }
+  return false;
+});
--- js/references.js
+++ js/references.js
@@ -1,4 +1,2 @@
-
-
 var file_name = document.getElementById('file-name');
 var edit_box = document.getElementById('edit-box');
