9154c03111074bab24d319eaec66b795336a84d6
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-02-07 15:03:42 +0100

    Update
    - Started implementation on dark/light mode
    - Fixed bug with file-name input

---
+++ js/theme.js
@@ -0,0 +1,44 @@
+// Store the theme
+let darkTheme = localStorage.getItem('dark_theme');
+const themeToggle = document.querySelector('#themeButton');
+const bodyBackground = document.getElementById('#body');
+
+// Apply Dark theme
+const enableDark = () => {
+  document.body.classList.add('darktheme');
+  localStorage.setItem('darkTheme', 'enabled');
+  themeToggle.innerHTML = `<i id="themeButton__icon" icon-name="sun"></i>`;
+  lucide.createIcons();
+};
+
+// Remove Dark theme
+const disableDark = () => {
+  document.body.classList.remove('darktheme');
+  localStorage.setItem('darkTheme', null);
+  themeToggle.innerHTML = `<i id="themeButton__icon" icon-name="moon"></i>`;
+  lucide.createIcons();
+};
+
+//Toggle theme
+if (darkTheme === 'enabled') {
+  // Temporarily disable transitions when changing theme on startup
+  document.body.classList.add('notransition');
+  enableDark();
+  document.body.offsetHeight; // Trigger reflow to flush CSS changes
+  document.body.classList.remove('notransition');
+} else {
+  disableDark();
+}
+
+themeToggle.addEventListener('click', () => {
+  darkTheme = localStorage.getItem('darkTheme');
+  if (darkTheme !== 'enabled') {
+    enableDark();
+  } else {
+    disableDark();
+  }
+});
+
+if (CONFIG.imageBackground) {
+  document.body.classList.add('withImageBackground');
+}
