98df3536f998a2fe82b649ea01dd3adce7e74bbd
Ruben Palmer Perez <RPP776@pas.uib.es>
2022-02-07 10:36:43 +0100

    Open & save buttons active

--- js/listeners.js
+++ js/listeners.js
@@ -4,18 +4,20 @@ addEvent(document.getElementById("edit-box"), 'input', save_settings);
 
 addEvent(document.getElementById("open-btn"), 'click', function () {
   if (window.File && window.FileReader && window.FileList && window.Blob) {
-    open_dilg.click();
+    document.getElementById("open-dilg").click();
   } else {
     alert('The File APIs are not fully supported in this browser.');
   }
   return false;
 });
 
-addEvent(save_btn, 'click', function () {
-  var filename = file_name.value;
+addEvent(document.getElementById("save-btn"), 'click', function () {
+  var filename = document.getElementById("file-name").value + ".txt";
   if (filename == '') {
     filename = 'untitled.txt';
   }
-  save_btn.download = filename;
-  save_btn.href = 'data:text/octet-stream,' + escape(edit_box.value);
+  document.getElementById("save-btn").download = filename;
+  document.getElementById("save-btn").href = 'data:text/octet-stream,' + escape(document.getElementById("file-name").value) + "\n" + escape(document.getElementById("edit-box").value);
 });
+
+addEvent(document.getElementById("open-dilg"), 'change', open_file);
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 function addEvent(elem, type, eventHandle) {
-    if (elem == null || typeof(elem) == 'undefined') return
+    if (elem == null || typeof (elem) == 'undefined') return
     if (elem.addEventListener) {
         elem.addEventListener(type, eventHandle, false)
     } else if (elem.attachEvent) {
@@ -7,4 +7,24 @@ function addEvent(elem, type, eventHandle) {
     } else {
         elem["on" + type] = eventHandle
     }
+}
+
+function open_file(e) {
+    var file = e.target.files;
+    document.getElementById("edit-box").value = '';
+    var reader = new FileReader();
+    reader.file = file[0]
+    reader.onload = function (e) {
+        lines = e.target.result.split("\n");
+
+        document.getElementById("file-name").value = lines[0];  // Name
+        
+        // I hate to iterate line by line to copy all the file. I could do something
+        // better, but I don't whant to use centinels to give the user max freedom
+        for (let i = 1; i < lines.length; i++) {
+            document.getElementById("edit-box").value += lines[i]; // File contents 
+        }
+    };
+    
+    reader.readAsText(file[0]);
 }
